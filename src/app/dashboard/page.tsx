6248193adf2509a6c468ba12bd236fdeff6c13d5
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2025-06-03 23:11:26 -0300

    feat: add clinic-form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignUpButton from "./components/sign-up-button";
@@ -13,6 +16,14 @@ const dashboardPage = async () => {
     redirect("/authentication");
   }
 
+  // Pegar clínicas do usuário
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
+  }
+  
   return (
     <div>
       <h1>Dashboard</h1>
